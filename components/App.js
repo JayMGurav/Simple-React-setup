ba9dad72dd5dff41fc1ab892a685777395f01234
Jay <mrithyunjay2799@gmail.com>
2019-08-12 09:29:16 +0530

    A simple react setup

---
+++ components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App(){
+    return(
+        <div className="padded">
+            <h1>
+                Hey this is awesome ✌
+            </h1>
+        </div>
+    );
+}
+
+export default App;
